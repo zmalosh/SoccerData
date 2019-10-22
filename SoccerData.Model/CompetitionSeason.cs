5d30c666c7dcbd0bb82be56206b8d1bc35343935
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2019-10-22 01:38:08 -0400

    add CompetitionSeason to context

---
+++ SoccerData.Model/CompetitionSeason.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerData.Model
+{
+	public class CompetitionSeason
+	{
+		public int CompetitionSeasonId { get; set; }
+		public int CompetitionId { get; set; }
+		public int Season { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public bool IsCurrent { get; set; }
+		public bool HasFixtures { get; set; }
+		public bool HasLineups { get; set; }
+		public bool HasPlayerStats { get; set; }
+		public bool HasTeamStats { get; set; }
+		public bool HasOdds { get; set; }
+		public bool HasPlayers { get; set; }
+		public bool HasPredictions { get; set; }
+		public bool HasStandings { get; set; }
+		public bool HasTopScorers { get; set; }
+
+		public virtual Competition Competition { get; set; }
+	}
+}
--- SoccerData.Model/SoccerDataContext.cs
+++ SoccerData.Model/SoccerDataContext.cs
@@ -12,6 +12,7 @@ namespace SoccerData.Model
 
 		public DbSet<Country> Countries { get; set; }
 		public DbSet<Competition> Competitions { get; set; }
+		public DbSet<CompetitionSeason> CompetitionSeasons { get; set; }
 
 		protected override void OnModelCreating(DbModelBuilder modelBuilder)
 		{
@@ -21,6 +22,10 @@ namespace SoccerData.Model
 			modelBuilder.Entity<Competition>().HasKey(c => c.CompetitionId);
 			modelBuilder.Entity<Competition>().Property(c => c.CompetitionId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
 			modelBuilder.Entity<Competition>().HasRequired(c => c.Country).WithMany(c => c.Competitions).HasForeignKey(c => c.CountryId);
+
+			modelBuilder.Entity<CompetitionSeason>().HasKey(cs => cs.CompetitionSeasonId);
+			modelBuilder.Entity<CompetitionSeason>().Property(cs => cs.CompetitionSeasonId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+			modelBuilder.Entity<CompetitionSeason>().HasRequired(cs => cs.Competition).WithMany(c => c.CompetitionSeasons).HasForeignKey(cs => cs.CompetitionId);
 		}
 	}
 }
