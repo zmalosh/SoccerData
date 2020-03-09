e308678ac5392807423f7d8e21c72303b70e4422
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-03-09 18:21:20 -0400

    remove db reset; get all leagues

--- SoccerData.Program/Tasks/InitializeFullLoadTask.cs
+++ SoccerData.Program/Tasks/InitializeFullLoadTask.cs
@@ -20,9 +20,9 @@ namespace SoccerData.Program.Tasks
 
 				context = new SoccerDataContext(config);
 
-				context.Database.EnsureDeleted();
-				context.Database.EnsureCreated();
-				context.SaveChanges();
+				//context.Database.EnsureDeleted();
+				//context.Database.EnsureCreated();
+				//context.SaveChanges();
 
 				var countriesProcessor = new CountriesProcessor();
 				Console.WriteLine("START COUNTRIES");
@@ -38,18 +38,19 @@ namespace SoccerData.Program.Tasks
 				context.SaveChanges();
 				Console.WriteLine("END LEAGUES");
 
-				var desiredLeagueIds = new List<int>
-				{
-					751, 752, 51, 403,							// EURO CHAMPS
-					1, 749, 750,								// WORLD CUP
-					52, 31, 132, 530, 53, 32, 137, 514,			// UEFA CL/EL
-					735, 734, 733, 732, 731, 730, 64, 30, 87,	// ESP - LA LIGA
-					65, 33, 88, 776,							// ESP - SEGUNDA
-					808, 973,									// ESP - COPA DEL REY
-					499, 498, 444, 968, 502, 501, 445, 966,		// AND - 1st AND 2nd
-					201, 200, 199, 294,							// USA - MLS
-					521, 520, 519, 518, 522, 523				// USA - USL
-				};
+				List<int> desiredLeagueIds = null;
+				//desiredLeagueIds = new List<int>
+				//{
+				//	751, 752, 51, 403,							// EURO CHAMPS
+				//	1, 749, 750,								// WORLD CUP
+				//	52, 31, 132, 530, 53, 32, 137, 514,			// UEFA CL/EL
+				//	735, 734, 733, 732, 731, 730, 64, 30, 87,	// ESP - LA LIGA
+				//	65, 33, 88, 776,							// ESP - SEGUNDA
+				//	808, 973,									// ESP - COPA DEL REY
+				//	499, 498, 444, 968, 502, 501, 445, 966,		// AND - 1st AND 2nd
+				//	201, 200, 199, 294,							// USA - MLS
+				//	521, 520, 519, 518, 522, 523				// USA - USL
+				//};
 				//desiredLeagueIds = new List<int>
 				//{
 				//	1, 749, 750,								// WORLD CUP
@@ -59,7 +60,7 @@ namespace SoccerData.Program.Tasks
 				//};
 
 				var competitionSeasonIds = context.CompetitionSeasons
-													.Where(x => desiredLeagueIds.Contains(x.ApiFootballId))
+													.Where(x => desiredLeagueIds == null || desiredLeagueIds.Contains(x.ApiFootballId))
 													.Select(x => x.CompetitionSeasonId)
 													.Distinct()
 													.OrderBy(x => x)
