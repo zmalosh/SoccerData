7fc20978f34bf32c8fa281bd2649abb31e74134e
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-03-14 12:56:47 -0400

    add blank FixtureFeed skeleton

---
+++ SoccerData.Processors/ApiFootball/Feeds/FixtureFeed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerData.Processors.ApiFootball.Feeds
+{
+	public class FixtureFeed
+	{
+		public static string GetFeedUrlByFixtureId(int apiFootballFixtureId)
+		{
+			return $"https://api-football-v1.p.rapidapi.com/v2/fixtures/id/{apiFootballFixtureId}";
+		}
+	}
+}
