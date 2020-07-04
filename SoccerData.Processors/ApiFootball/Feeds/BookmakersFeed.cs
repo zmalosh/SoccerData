df3146553cdb2399069dbaa5f3b8dcb8ad54a4a7
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-07-04 17:53:08 -0400

    add bookmakers feed

---
+++ SoccerData.Processors/ApiFootball/Feeds/BookmakersFeed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SoccerData.Processors.ApiFootball.Feeds
+{
+	public class BookmakersFeed
+	{
+		public static string GetFeedUrl()
+		{
+			return $"https://api-football-v1.p.rapidapi.com/v2/odds/bookmakers/";
+		}
+
+		public static BookmakersFeed FromJson(string json) => JsonConvert.DeserializeObject<BookmakersFeed>(json, Converter.Settings);
+
+		[JsonProperty("api")]
+		public ApiResult Result { get; set; }
+
+		public class ApiResult
+		{
+			[JsonProperty("results")]
+			public int Count { get; set; }
+
+			[JsonProperty("bookmakers")]
+			public List<ApiBookmaker> Rounds { get; set; }
+		}
+
+		public class ApiBookmaker
+		{
+			[JsonProperty("id")]
+			public int Id { get; set; }
+
+			[JsonProperty("name")]
+			public string Name { get; set; }
+		}
+	}
+
+	public static partial class Serialize
+	{
+		public static string ToJson(this BookmakersFeed self) => JsonConvert.SerializeObject(self, Converter.Settings);
+	}
+}
